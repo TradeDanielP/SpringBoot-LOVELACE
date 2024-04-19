637728507f426006d8f06339d3b4dc1a39549ce6
TradeDanielP <tradedanielp@gmail.com>
2024-04-19 11:45:28 -0500

    Crud y taller 1

--- _02_PrimeraWeb/src/main/java/com/riwi/primeraweb/controller/CoderController.java
+++ _02_PrimeraWeb/src/main/java/com/riwi/primeraweb/controller/CoderController.java
@@ -3,63 +3,91 @@ package com.riwi.primeraweb.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.riwi.primeraweb.entity.Coder;
 import com.riwi.primeraweb.service.CoderService;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 @Controller
-//Crear la ruta donde se activara este controlador
+// Crear la ruta donde se activara este controlador
 @RequestMapping("/")
 public class CoderController {
     // Establecer la inyeccion de dependencias
     @Autowired
     private CoderService objCoderService;
 
-    //Metodo para mostrar la vista y enviarle toda la lista de Coders
+    // Metodo para mostrar la vista y enviarle toda la lista de Coders
     @GetMapping()
-    public String showViewCoder(Model objModel){
+    public String showViewCoder(
+            Model objModel,
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "2") int size) {
 
-        //Obtenemos la lista de COders
-        List<Coder> listCoders = this.objCoderService.findAll();
+        // Obtenemos la lista de COders
+        Page<Coder> listCoders = this.objCoderService.findAllPaginate(page - 1,size);
 
-        //Cargamos la lista en modelo}
-        objModel.addAttribute("listCoders", listCoders); //Llave - Valor
+        // Cargamos la lista en modelo}
+        objModel.addAttribute("listCoders", listCoders); // Llave - Valor
+        objModel.addAttribute("currentPage", page);
+        objModel.addAttribute("totalPage", listCoders.getTotalPages());
 
         return "viewCoders";
     }
 
+    // Metodo para enviar el Objeto vacio para despues ser llenado por la accion del
+    // form
+    // Get Mapping es para mostrar las vistas
     @GetMapping("/form")
-    public String showViewForm(){
-
+    public String showViewForm(Model model) {
+        model.addAttribute("coder", new Coder());
+        model.addAttribute("action", "/create-coder");
         return "viewForm";
     }
 
-    //Metodo para recibir toda la informacion del Formulario
-    //MOdelAttribute para recibir informacion de la vista
+    // Metodo para recibir toda la informacion del Formulario
+    // MOdelAttribute para recibir informacion de la vista
     @PostMapping("create-coder")
-    public String createCoder(@ModelAttribute Coder objCoder){
-
+    public String createCoder(@ModelAttribute Coder objCoder) {
         this.objCoderService.create(objCoder);
 
         return "redirect:/";
     }
 
-    //Metodo para mostrar la vista de formulario y ademas enviar una instancia de Coder vacia
-    @PostMapping("form")
-    public String showViewForm(Model model){
+    // Controlador para eliminar recibira como parametro el id por URL
+    // PathVariable funciona para obtener el valor de una variable de una url, solo
+    // si es de tipo path (ejm: /delete/10)
+    // donde el 10 es dinamico
+    @GetMapping("/delete/{id}")
+    public String deleteCoder(@PathVariable Long id) {
+        this.objCoderService.delete(id);
 
-        model.addAttribute("coder", new Coder());
-        model.addAttribute("action", "/create-coder");
+        // Redireccionar a la lista de coders
+        return "redirect:/";
+    }
+
+    // Controlador para mostrar el formulario con la informacion rellenada
+    @GetMapping("/update/{id}")
+    public String updateCoder(@PathVariable Long id, Model model) {
+        Coder objCoder = this.objCoderService.findCoderById(id);
+
+        model.addAttribute("coder", objCoder);
+        model.addAttribute("action", "/edit/" + id);
 
         return "viewForm";
+    }
+
+    @PostMapping("edit/{id}")
+    public String updateCoder(@PathVariable Long id, @ModelAttribute Coder objCoder) {
+        this.objCoderService.updateCoder(id, objCoder);
+        return "redirect:/";
     }
 
 }
--- _02_PrimeraWeb/src/main/java/com/riwi/primeraweb/service/CoderService.java
+++ _02_PrimeraWeb/src/main/java/com/riwi/primeraweb/service/CoderService.java
@@ -3,6 +3,9 @@ package com.riwi.primeraweb.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.riwi.primeraweb.entity.Coder;
@@ -14,14 +17,53 @@ public class CoderService {
     // Registra la inyeccion de dependencias en SpringBoot
     @Autowired
     private CoderRepository objCoderRepository;
-    
-    public List<Coder> findAll(){
+
+    public List<Coder> findAll() {
 
         return this.objCoderRepository.findAll();
     }
 
-    //Metodo para crear un nuevo Coder, se hace uso del repositorio y del metodo Save, el cual se encarga de insertar
-    public Coder create(Coder objCoder){
+    public Page<Coder> findAllPaginate(int page, int size){
+        //Validar que la pagina no sea menor a 0
+        if (page < 0) {
+            page = 0;
+        }
+        //Creamos la paginacion
+        Pageable objPage = PageRequest.of(page, size);
+        // Retornamos una lista paginada de Coder
+        return this.objCoderRepository.findAll(objPage);
+    }
+
+    // Metodo para crear un nuevo Coder, se hace uso del repositorio y del metodo
+    // Save, el cual se encarga de insertar
+    public Coder create(Coder objCoder) {
         return this.objCoderRepository.save(objCoder);
+    }
+
+    // Metodo para eliminar un Coder (deleteById)
+    public void delete(Long id) {
+        // lamar al repositorio
+        this.objCoderRepository.deleteById(id);
+    }
+
+    // Metodo para obtener por ID un Coder
+    public Coder findCoderById(Long id) {
+        // Busca en un Coder por ID y si no lo encuentra retorna null, si lo encuentra
+        // retorna un objeto, una instancia de Coder
+        return this.objCoderRepository.findById(id).orElse(null);
+    }
+
+    //Metodo para actualizar un Coder
+    public Coder updateCoder(Long id, Coder coder){
+
+        //1.Buscar coder por ID
+        Coder objCoderDB = this.findCoderById(id);
+        //2. Verificamos que el coder existe
+        if (objCoderDB == null) return null;
+        //3. Actualizamos el Coder Antiguo
+        objCoderDB = coder;
+        //4. Retornamos el Coder con el metodo save que verifica si la llave primaria llega llena
+        // y depende eso, actualiza o crea
+        return this.objCoderRepository.save(objCoderDB);
     }
 }
